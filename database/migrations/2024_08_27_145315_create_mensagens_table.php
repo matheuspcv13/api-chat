4d8448f47203b0da8b1b1e74eb2bb08a63af3451
thiagopcvv <thiagopiresdovalle@hotmail.com>
2024-09-06 10:45:41 -0400

    fix: fixign column table

--- database/migrations/2024_08_27_145315_create_mensagens_table.php
+++ database/migrations/2024_08_27_145315_create_mensagens_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->foreignId('conversa_id')->constrained()->onDelete('cascade');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->text('mensagem');
-            $table->file('image');
+            $table->text('image');
             $table->timestamps();
         });
     }
