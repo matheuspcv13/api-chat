a283feeda0e7a8b46f0815417686a439411d8f1b
thiagopcvv <thiagopiresdovalle@hotmail.com>
2024-09-13 08:58:21 -0400

    fix: name object friendship

--- app/Http/Controllers/FriendshipController.php
+++ app/Http/Controllers/FriendshipController.php
@@ -19,7 +19,7 @@ class FriendshipController extends Controller
 
         return response()->json([
             'friends' => $friendships,
-            'pendig' => $friendPending
+            'pending' => $friendPending
         ]);
     } 
 
